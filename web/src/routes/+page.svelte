5228153a378d9339bc90e4aabdf7f3500c03a506
Matthew J. Moran <2759007+mattjmoran@users.noreply.github.com>
2025-01-01 00:54:59 -0600

    Removed CNAME, adjusted paragraph text to reflect use of Cloudflare Pages

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1>Welcome to matthewjmoran.com</h1>
-<p>This website was built using SvelteKit's static site generator and hosted on GitHub pages.</p>
+<p>
+	This website was built using SvelteKit's static site generator and hosted on Cloudflare Pages.
+</p>
