a8b0ab7df94ec22d56f96c6351fb8dc636a63b10
Matthew J. Moran <2759007+mattjmoran@users.noreply.github.com>
2025-02-17 09:22:22 -0600

    feat: enhance pointer visibility and hover effects with animations

--- src/lib/components/Pointer.svelte
+++ src/lib/components/Pointer.svelte
@@ -2,6 +2,8 @@
 	import { Spring } from 'svelte/motion';
 	import { scale } from 'svelte/transition';
 
+	let isVisible = $state(false);
+	let isHover = $state(false);
 	let pointer = $state(
 		new Spring(
 			{ x: 0, y: 0 },
@@ -11,7 +13,6 @@
 			}
 		)
 	);
-	let visible = $state(false);
 
 	const norm = (val: number, max: number, min: number) => (val - min) / (max - min);
 	let hypot = $derived(
@@ -19,20 +20,30 @@
 	);
 	let border = $derived(6 * Math.max(1 - norm(hypot, 100, 0), 0) + 2);
 
-	const onmousemove = (event: { pageX: number; pageY: number }) => {
-		visible = true;
-		pointer.target = { x: event.pageX, y: event.pageY };
+	const onpointermove = (event: { pointerType: string; pageX: number; pageY: number; target: EventTarget | null }) => {
+		if (event.pointerType == 'mouse') {
+			pointer.target = { x: event.pageX, y: event.pageY };
+			isVisible = true;
+			if (event.target instanceof Element) {
+				const tagName = event.target.tagName.toLowerCase();
+				isVisible = !['p', 'span'].includes(tagName);
+				isHover = ['a', 'button'].includes(tagName);
+			}
+		} else {
+			isVisible = false;
+		}
 	};
-	const onscroll = () => (visible = false);
-	const onmouseleave = () => (visible = false);
+	const onscroll = () => (isVisible = false);
+	const onpointerleave = () => (isVisible = false);
 </script>
 
 <svelte:window {onscroll} />
-<svelte:body {onmousemove} {onmouseleave} />
+<svelte:body {onpointermove} {onpointerleave} />
 
-{#if visible}
+{#if isVisible}
 	<div
 		transition:scale={{ duration: 500 }}
+		class:hover={isHover}
 		style="left:{pointer.current.x}px; top:{pointer.current.y}px; border-width:{border}px"
 	></div>
 {/if}
@@ -42,15 +53,27 @@
 		--size: 40px;
 		width: var(--size);
 		height: var(--size);
-
 		position: absolute;
 		transform: translate(-50%, -50%);
-
 		border-radius: 50%;
 		border-style: solid;
 		border-color: var(--green);
-
 		pointer-events: none;
 		z-index: var(--z-pointer);
+	}
+
+	@keyframes rotating {
+  from {
+    transform: translate(-50%, -50%) rotate(0deg);
+  }
+  to {
+    transform: translate(-50%, -50%) rotate(360deg);
+  }
+}
+
+	.hover {
+		animation: rotating 3s linear infinite;
+		border-style: dashed;
+		border-width: 3px !important;
 	}
 </style>
