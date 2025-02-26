61908f436ed711a6efa347a7adab862c1884529c
Matthew J. Moran <2759007+mattjmoran@users.noreply.github.com>
2025-02-25 20:07:26 -0600

    feat: change loading component to use icons and improved transitions

--- src/lib/components/Loading.svelte
+++ src/lib/components/Loading.svelte
@@ -1,88 +1,62 @@
 <script lang="ts">
-	import { fade, slide } from 'svelte/transition';
-	import { quintInOut } from 'svelte/easing';
+	import { fade } from 'svelte/transition';
+	import animate from '$lib/utils/animate.svelte';
+	import Icon from '$lib/components/Icon.svelte';
 
 	let { loading }: { loading: boolean } = $props();
 
-	type Square = {
-		width: number;
-		height: number;
-		x: number;
-		y: number;
-		begin: number;
-	};
+	const loadingIcons = ['computer', 'compass', 'lightbulb'];
 
-	const squareSize = 15;
-	const borderSize = 5;
-	const width = 3 * squareSize + 2 * borderSize;
-	const height = 3 * squareSize + 2 * borderSize;
-	const dur = 1;
-	const order = [
-		{ row: 0, col: 1 }, // Top-center
-		{ row: 0, col: 2 }, // Top-right
-		{ row: 1, col: 2 }, // Middle-right
-		{ row: 2, col: 2 }, // Bottom-right
-		{ row: 2, col: 1 }, // Bottom-center
-		{ row: 2, col: 0 }, // Bottom-left
-		{ row: 1, col: 0 }, // Middle-left
-		{ row: 0, col: 0 } // Top-left
-	];
-
-	const squares: Square[] = [];
-	order.forEach((pos, index) => {
-		squares.push({
-			width: squareSize,
-			height: squareSize,
-			x: pos.col * (squareSize + borderSize),
-			y: pos.row * (squareSize + borderSize),
-			begin: -(dur - index * (dur / order.length))
-		});
-	});
+	let transitionDuration = animate.delay * (0.1);
+	let transitionDelay = animate.delay * (0.9);
 </script>
 
 {#if loading}
-	<div transition:slide={{ duration: 1500, delay: 1000, axis: 'x', easing: quintInOut }}>
-		<svg
-			transition:fade={{ duration: 500, delay: 1000 }}
-			{width}
-			{height}
-			viewBox="0 0 {width} {height}"
-			xmlns="http://www.w3.org/2000/svg"
-			fill="#fff"
-		>
-			{#each squares as square}
-				<rect {...square} style="animation-delay:{square.begin}s; --dur:{dur}s;" />
-			{/each}
-		</svg>
+	<div class="loading" transition:fade={{ duration: transitionDuration, delay: transitionDelay }}>
+		{#each loadingIcons as icon, index}
+			<div class="icon" style="animation-delay: {200 * index}ms">
+				<Icon name={icon} size="100px" fill="#fff" />
+			</div>
+		{/each}
 	</div>
 {/if}
 
-<style>
-	div {
+<style lang="postcss">
+	@import '$lib/styles/extends.pcss';
+
+	@keyframes switchIcons {
+		0% {
+			opacity: 1;
+		}
+		33% {
+			opacity: 0;
+		}
+		66% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+
+	.loading {
+		@extend %flex-center;
 		z-index: var(--z-loading);
 		position: fixed;
 		inset: 0;
 		width: 100%;
 		height: 100%;
-		display: grid;
-		place-items: center;
 		overflow: hidden;
 		background: black;
 	}
 
-	@keyframes fade {
-		0% {
-			fill-opacity: 1;
-		}
-		50% {
-			fill-opacity: 0.25;
-		}
-		100% {
-			fill-opacity: 1;
-		}
+	.icon {
+		position: absolute;
+		animation: switchIcons 600ms steps(1, end) infinite;
+		opacity: 0;
 	}
 
-	rect {
-		animation: fade var(--dur) ease-in-out infinite;
+	.icon:nth-of-type(1) {
+		opacity: 1;
 	}
 </style>
