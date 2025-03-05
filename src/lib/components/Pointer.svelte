32dbf7cc03bfd87dd3f09a6aaf6ac614805fc50a
Matthew J. Moran <2759007+mattjmoran@users.noreply.github.com>
2025-03-04 20:09:41 -0600

    refactor: refactor Pointer.svelte for improved readability and consistency in event handling logic

--- src/lib/components/Pointer.svelte
+++ src/lib/components/Pointer.svelte
@@ -7,36 +7,36 @@
 	// Reactive State Variables
 	let isVisible = $state(false);
 	let isHovering = $state(false);
-
 	let follower = $state(new Spring({ x: 0, y: 0 }, { stiffness: 0.1, damping: 0.5 }));
 
 	// Utility Functions
-	const normalize = (value: number, max: number, min: number) => (value - min) / (max - min);
+	const normalize = (value: number, max: number, min: number) => {
+		return (value - min) / (max - min);
+	};
 
 	// Derived Values
 	let distance = $derived(
 		Math.hypot(follower.current.x - follower.target.x, follower.current.y - follower.target.y)
 	);
-
 	let borderWidth = $derived(6 * Math.max(1 - normalize(distance, 100, 0), 0) + 2);
 
 	// Event Handlers
 	const onpointermove = (event: PointerEvent) => {
 		const { pointerType, pageX, pageY, target } = event;
-		pointer.x = pageX;
-		pointer.y = pageY;
+		[pointer.x, pointer.y] = [pageX, pageY];
 		pointer.type = pointerType;
 
 		if (pointerType === 'mouse') {
-			follower.target = { x: pageX, y: pageY };
 			isVisible = true;
+			follower.target = { x: pageX, y: pageY };
 			if (target && target instanceof Element) {
 				const cursor = getComputedStyle(target).cursor;
 				isHovering = cursor === 'pointer' || cursor === 'auto';
 			}
+		} else {
+			isVisible = false;
 		}
 	};
-
 	const onscroll = () => (isVisible = false);
 	const onpointerleave = () => (isVisible = false);
 </script>
@@ -54,9 +54,9 @@
 
 <style>
 	div {
-		--size: 40px;
-		width: var(--size);
-		height: var(--size);
+		--follower-size: 40px;
+		width: var(--follower-size);
+		height: var(--follower-size);
 		position: absolute;
 		transform: translate(-50%, -50%);
 		border-radius: 50%;
